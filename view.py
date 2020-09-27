ac575e4b7f1bf1688847fc56c8aefe604d5a96aa
mixichao <9711771923@qq.com>
2020-09-27 08:22:50 -0700

    tianjia

--- view.py
+++ view.py
@@ -1,6 +1,8 @@
 from diango.http import HttpResponse
-
+from diango.shortsuts import redirect
 
 def index(request):
     return HttpResponse('index')
 
+def login(request):
+    return redirect('/index')
